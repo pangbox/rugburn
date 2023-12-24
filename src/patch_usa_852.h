c8325ecdd3e92d172a9e8666f4421e01ae7ebf70
Acrisio Filho <acrisio_299@hotmail.com>
2023-12-24 11:16:45 -0300

    Patch ProjectG USA 852.00
    Signed-off-by: Acrisio Filho <acrisio_299@hotmail.com>

---
+++ src/patch_usa_852.h
@@ -0,0 +1,12 @@
+#ifndef PATCH_USA_852_H
+#define PATCH_USA_852_H
+
+#include "common.h"
+#include <stdint.h>
+
+// C not have __thiscall
+#define __thiscall __fastcall
+
+void __thiscall OnUnderBar_RankingUp(void* _this);
+
+#endif
