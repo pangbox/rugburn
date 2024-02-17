7c64e208490d22e39857488a936feda54d8134ca
John Chadwick <john@jchw.io>
2024-02-17 04:39:34 -0500

    Updates, nix flake cleanup, .envrc, etc.

--- flake.nix
+++ flake.nix
@@ -11,45 +11,54 @@
       let
         pkgs = nixpkgs.legacyPackages.${system};
         common = {
-          nativeBuildInputs = with pkgs; [ makeWrapper go open-watcom-v2 ];
+          name = "rugburn";
           src = self;
+          nativeBuildInputs = with pkgs; [
+            makeWrapper
+            go_1_22
+            gopls
+            open-watcom-v2
+            clang-tools
+          ];
           buildPhase = ''
             make
           '';
+          installPhase = ''
+            install -D -t $out/lib out/rugburn.dll
+            cp -r web/dist $out/dist
+          '';
           checkPhase = "";
+          vendorHash = (pkgs.lib.fileContents ./go.mod.sri);
           WATCOM = "${pkgs.open-watcom-v2.out}";
-          vendorHash = "sha256-zIJxqzc9C9tFWMm8R4dvkRmSh7mtfdI95WuzU+My10w=";
         };
-      in rec {
-        packages = rec {
-          default = pkgs.buildGoModule (common // {
-            name = "rugburn";
-            installPhase = "install -D -t $out/lib out/rugburn.dll";
-          });
-          web = pkgs.buildGoModule (common // {
-            name = "rugburn-web";
-            installPhase = "mkdir -p $out && cp -r web/dist $out";
-          });
-          dockerImage = pkgs.dockerTools.buildImage {
-            name = "rugburn-docker";
-            config = {
-              Cmd = [
-                "${pkgs.caddy}/bin/caddy"
-                "run"
-                "--adapter"
-                "caddyfile"
-                "--config"
-                (pkgs.writeText "caddyfile"
-                ''
-                :8080
-                root * ${web.out}/dist
-                file_server
-                '')
-              ];
-            };
-          };
-        };
+        rugburn = pkgs.buildGo122Module common;
         devShell = pkgs.mkShell common;
+        setupClangd = pkgs.writeShellScriptBin "setup-clangd.sh" ''
+          export WATCOM="${pkgs.open-watcom-v2.out}";
+          exec ${./scripts/setup-clangd.sh} "$@"
+        '';
+        updateVendorHash = pkgs.writeShellScriptBin "update-vendor-hash.sh" ''
+          export PATH="${pkgs.lib.makeBinPath [ pkgs.go_1_22 ]}:$PATH"
+          exec ${./scripts/update-vendor-hash.sh} "$@"
+        '';
+        caddyfile = pkgs.writeText "caddyfile" ''
+          :8080
+          root * ${rugburn}/dist
+          file_server
+        '';
+        web = pkgs.writeShellScriptBin "web.sh" ''
+          exec ${pkgs.caddy}/bin/caddy run --adapter caddyfile --config ${caddyfile}
+        '';
+        dockerImage = pkgs.dockerTools.buildImage {
+          name = "rugburn-docker";
+          config.Cmd = [ "${web}/bin/web.sh" ];
+        };
+      in {
+        inherit devShell;
+        packages = {
+          inherit rugburn setupClangd updateVendorHash web dockerImage;
+          default = rugburn;
+        };
       }
     );
 }
--- go.mod
+++ go.mod
@@ -1,10 +1,11 @@
 module github.com/pangbox/rugburn
 
-go 1.19
-
-require golang.org/x/exp v0.0.0-20230210204819-062eb4c674ab
+go 1.22.0
 
 require (
-	github.com/fsnotify/fsnotify v1.6.0 // indirect
-	golang.org/x/sys v0.1.0 // indirect
+	github.com/fsnotify/fsnotify v1.7.0
+	golang.org/x/exp v0.0.0-20240119083558-1b970713d09a
+	tailscale.com v1.60.0
 )
+
+require golang.org/x/sys v0.16.0 // indirect
