9ae90d593e39205896006e70f4383d5e08403631
Acrisio Fragoso Vieira Filho <92784292+Acrisio-Filho@users.noreply.github.com>
2024-06-23 18:10:35 -0300

    Update msvcr100.c

--- src/hooks/msvcr100/msvcr100.c
+++ src/hooks/msvcr100/msvcr100.c
@@ -4,7 +4,7 @@
 typedef int (__cdecl* STRICMPFNPTR)(const char *s1, const char *s2);
 typedef char *(__cdecl* SETLOCALEFNPTR)(int category, const char *locale);
 
-static HMODULE hMsvcr100Module = NULL;
+static HMODULE hMsvcrModule = NULL;
 static STRICMPFNPTR pStricmp = NULL;
 static SETLOCALEFNPTR pSetLocale = NULL;
 
@@ -27,14 +27,14 @@ VOID InitMsvcr100Hook() {
     const char *oldlocale;
     int result;
 
-    hMsvcr100Module = LoadLib("msvcr100");
-    if (!hMsvcr100Module) {
+    hMsvcrModule = LoadLib("msvcr100");
+    if (!hMsvcrModule) {
         Log("Not checking for Wine _stricmp bug: msvcr100 not found.\n");
         return;
     }
 
-    pStricmp = GetProc(hMsvcr100Module, "_stricmp");
-    pSetLocale = GetProc(hMsvcr100Module, "setlocale");
+    pStricmp = GetProc(hMsvcrModule, "_stricmp");
+    pSetLocale = GetProc(hMsvcrModule, "setlocale");
     oldlocale = pSetLocale(2, NULL);
     if (!pStricmp || !pSetLocale) {
         Log("Not checking for Wine _stricmp bug: msvcr100 functions not found.\n");
@@ -48,9 +48,44 @@ VOID InitMsvcr100Hook() {
 
     if (result == 0) {
         Log("Wine _stricmp bug detected; mitigating.\n");
-        pStricmp = HookProc(hMsvcr100Module, "_stricmp", StricmpHook);
+        pStricmp = HookProc(hMsvcrModule, "_stricmp", StricmpHook);
     } else {
         Log("Wine _stricmp bug not detected.\n");
     }
 }
 
+VOID InitMsvcr71Hook() {
+    const char *oldlocale;
+    int result;
+
+    hMsvcrModule = LoadLib("msvcr71");
+    if (!hMsvcrModule) {
+        Log("Not checking for Wine _stricmp bug: msvcr71 not found.\n");
+        return;
+    }
+
+    pStricmp = GetProc(hMsvcrModule, "_stricmp");
+    pSetLocale = GetProc(hMsvcrModule, "setlocale");
+    oldlocale = pSetLocale(2, NULL);
+    if (!pStricmp || !pSetLocale) {
+        Log("Not checking for Wine _stricmp bug: msvcr71 functions not found.\n");
+    }
+
+    Log("Checking for Wine _stricmp bug; current locale: %s - Temporarily switching to Kor.\n", oldlocale);
+
+    pSetLocale(2, "Kor");
+    result = pStricmp("\xB3\xA1", "\xBC\xBC");
+    pSetLocale(2, oldlocale);
+
+    if (result == 0) {
+        Log("Wine _stricmp bug detected; mitigating.\n");
+        pStricmp = HookProc(hMsvcrModule, "_stricmp", StricmpHook);
+    } else {
+        Log("Wine _stricmp bug not detected.\n");
+    }
+}
+
+VOID InitMsvcrHook() {
+    InitMsvcr71Hook();
+    InitMsvcr100Hook();
+}
