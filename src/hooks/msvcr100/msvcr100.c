ec4ae0e22adb9e2e09b896ec87ef69260abc3f11
John Chadwick <john@jchw.io>
2024-07-22 23:26:40 -0400

    Add license headers + fix subdir formatting

--- src/hooks/msvcr100/msvcr100.c
+++ src/hooks/msvcr100/msvcr100.c
@@ -1,23 +1,39 @@
+/**
+ * Copyright 2018-2024 John Chadwick <john@jchw.io>
+ *
+ * Permission to use, copy, modify, and/or distribute this software for any purpose
+ * with or without fee is hereby granted, provided that the above copyright notice
+ * and this permission notice appear in all copies.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES WITH
+ * REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF MERCHANTABILITY AND
+ * FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY SPECIAL, DIRECT,
+ * INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES WHATSOEVER RESULTING FROM LOSS
+ * OF USE, DATA OR PROFITS, WHETHER IN AN ACTION OF CONTRACT, NEGLIGENCE OR OTHER
+ * TORTIOUS ACTION, ARISING OUT OF OR IN CONNECTION WITH THE USE OR PERFORMANCE OF
+ * THIS SOFTWARE.
+ */
+
 #include "msvcr100.h"
 #include "../../patch.h"
 
-typedef int (__cdecl* STRICMPFNPTR)(const char *s1, const char *s2);
-typedef char *(__cdecl* SETLOCALEFNPTR)(int category, const char *locale);
+typedef int(__cdecl *STRICMPFNPTR)(const char *s1, const char *s2);
+typedef char *(__cdecl *SETLOCALEFNPTR)(int category, const char *locale);
 
 static HMODULE hMsvcrModule = NULL;
 static STRICMPFNPTR pStricmp = NULL;
 static SETLOCALEFNPTR pSetLocale = NULL;
 
-int lower(int c) {
-    return (c >= 'A' && c <= 'Z') ? c + 'a' - 'A' : c;
-}
+int lower(int c) { return (c >= 'A' && c <= 'Z') ? c + 'a' - 'A' : c; }
 
 int __cdecl StricmpHook(const char *s1, const char *s2) {
     int result = 0;
-    if (!s1 || !s2) return 0x7fffffff;
+    if (!s1 || !s2)
+        return 0x7fffffff;
     while (!result) {
         result = lower(*s1) - lower(*s2);
-        if (!*s1 || !*s2) break;
+        if (!*s1 || !*s2)
+            break;
         s1++, s2++;
     }
     return result;
@@ -40,7 +56,9 @@ VOID InitMsvcr100Hook() {
         Log("Not checking for Wine _stricmp bug: msvcr100 functions not found.\n");
     }
 
-    Log("Checking for Wine _stricmp bug in msvcr100; current locale: %s - Temporarily switching to Kor.\n", oldlocale);
+    Log("Checking for Wine _stricmp bug in msvcr100; current locale: %s - Temporarily switching to "
+        "Kor.\n",
+        oldlocale);
 
     pSetLocale(2, "Kor");
     result = pStricmp("\xB3\xA1", "\xBC\xBC");
@@ -71,7 +89,9 @@ VOID InitMsvcr71Hook() {
         Log("Not checking for Wine _stricmp bug: msvcr71 functions not found.\n");
     }
 
-    Log("Checking for Wine _stricmp bug in msvcr71; current locale: %s - Temporarily switching to Kor.\n", oldlocale);
+    Log("Checking for Wine _stricmp bug in msvcr71; current locale: %s - Temporarily switching to "
+        "Kor.\n",
+        oldlocale);
 
     pSetLocale(2, "Kor");
     result = pStricmp("\xB3\xA1", "\xBC\xBC");
