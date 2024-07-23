9e3f4db4a77f45affabb518dd64331afdb0772dd
John Chadwick <john@jchw.io>
2024-07-22 23:26:40 -0400

    Add license headers + fix subdir formatting

--- src/hooks/user32/window.c
+++ src/hooks/user32/window.c
@@ -1,3 +1,19 @@
+/**
+ * Copyright 2018-2024 John Chadwick <john@jchw.io>
+ *
+ * Permission to use, copy, modify, and/or distribute this software for any purpose
+ * with or without fee is hereby granted, provided that the above copyright notice
+ * and this permission notice appear in all copies.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES WITH
+ * REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF MERCHANTABILITY AND
+ * FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY SPECIAL, DIRECT,
+ * INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES WHATSOEVER RESULTING FROM LOSS
+ * OF USE, DATA OR PROFITS, WHETHER IN AN ACTION OF CONTRACT, NEGLIGENCE OR OTHER
+ * TORTIOUS ACTION, ARISING OUT OF OR IN CONNECTION WITH THE USE OR PERFORMANCE OF
+ * THIS SOFTWARE.
+ */
+
 #include "window.h"
 #include "../../patch.h"
 
@@ -8,9 +24,13 @@ static PFNCREATEWINDOWEXAPROC pCreateWindowExA = NULL;
  * CreateWindowExAHook is a convenience patch that prevents PangYa from
  * creating topmost windows.
  */
-HWND STDCALL CreateWindowExAHook(DWORD dwExStyle, LPCSTR lpClassName, LPCSTR lpWindowName, DWORD dwStyle, int X, int Y, int nWidth, int nHeight, HWND hWndParent, HMENU hMenu, HINSTANCE hInstance, LPVOID lpParam) {
+HWND STDCALL CreateWindowExAHook(DWORD dwExStyle, LPCSTR lpClassName, LPCSTR lpWindowName,
+                                 DWORD dwStyle, int X, int Y, int nWidth, int nHeight,
+                                 HWND hWndParent, HMENU hMenu, HINSTANCE hInstance,
+                                 LPVOID lpParam) {
     dwExStyle &= (~WS_EX_TOPMOST);
-    return pCreateWindowExA(dwExStyle, lpClassName, lpWindowName, dwStyle, X, Y, nWidth, nHeight, hWndParent, hMenu, hInstance, lpParam);
+    return pCreateWindowExA(dwExStyle, lpClassName, lpWindowName, dwStyle, X, Y, nWidth, nHeight,
+                            hWndParent, hMenu, hInstance, lpParam);
 }
 
 VOID InitWindowHook() {
--- src/hooks/wininet/netredir.c
+++ src/hooks/wininet/netredir.c
@@ -1,6 +1,22 @@
+/**
+ * Copyright 2018-2024 John Chadwick <john@jchw.io>
+ *
+ * Permission to use, copy, modify, and/or distribute this software for any purpose
+ * with or without fee is hereby granted, provided that the above copyright notice
+ * and this permission notice appear in all copies.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES WITH
+ * REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF MERCHANTABILITY AND
+ * FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY SPECIAL, DIRECT,
+ * INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES WHATSOEVER RESULTING FROM LOSS
+ * OF USE, DATA OR PROFITS, WHETHER IN AN ACTION OF CONTRACT, NEGLIGENCE OR OTHER
+ * TORTIOUS ACTION, ARISING OUT OF OR IN CONNECTION WITH THE USE OR PERFORMANCE OF
+ * THIS SOFTWARE.
+ */
+
 #include "netredir.h"
-#include "../../patch.h"
 #include "../../config.h"
+#include "../../patch.h"
 
 HMODULE hWinINet = NULL;
 PFNINTERNETOPENURLAPROC pInternetOpenUrlA = NULL;
@@ -8,18 +24,21 @@ PFNINTERNETOPENURLAPROC pInternetOpenUrlA = NULL;
 /**
  * InternetOpenUrlAHook redirects HTTP calls to localhost.
  */
-HINTERNET STDCALL InternetOpenUrlAHook(HINTERNET hInternet, LPCSTR lpszUrl, LPCSTR lpszHeaders, DWORD dwHeadersLength, DWORD dwFlags, DWORD_PTR dwContext) {
+HINTERNET STDCALL InternetOpenUrlAHook(HINTERNET hInternet, LPCSTR lpszUrl, LPCSTR lpszHeaders,
+                                       DWORD dwHeadersLength, DWORD dwFlags, DWORD_PTR dwContext) {
     LPCSTR newURL;
     HINTERNET hResult;
 
     newURL = RewriteURL(lpszUrl);
     if (newURL != NULL) {
         Log("InternetOpenUrlA(%s -> %s)\r\n", lpszUrl, newURL);
-        hResult = pInternetOpenUrlA(hInternet, newURL, lpszHeaders, dwHeadersLength, dwFlags, dwContext);
+        hResult =
+            pInternetOpenUrlA(hInternet, newURL, lpszHeaders, dwHeadersLength, dwFlags, dwContext);
         FreeMem((HLOCAL)newURL);
     } else {
         Log("InternetOpenUrlA(%s) // (no rewrite rules matched)\r\n", lpszUrl, newURL);
-        hResult = pInternetOpenUrlA(hInternet, newURL, lpszHeaders, dwHeadersLength, dwFlags, dwContext);
+        hResult =
+            pInternetOpenUrlA(hInternet, newURL, lpszHeaders, dwHeadersLength, dwFlags, dwContext);
     }
     return hResult;
 }
