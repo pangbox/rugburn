c8490da93300389e34a6e979b76c833e9bf0cf1f
John Chadwick <john@jchw.io>
2024-08-05 23:49:20 -0400

    Fix formatting errors in ijl15.c

--- src/ijl15.c
+++ src/ijl15.c
@@ -1,17 +1,11 @@
-#include <windows.h>
 #include "../third_party/ijl/ijl.h"
+#include <windows.h>
 
-const IJLibVersion *__stdcall ijlGetLibVersionWrapper() {
-    return ijlGetLibVersion();
-}
+const IJLibVersion *__stdcall ijlGetLibVersionWrapper() { return ijlGetLibVersion(); }
 
-IJLERR __stdcall ijlInitWrapper(JPEG_CORE_PROPERTIES *jcprops) {
-    return ijlInit(jcprops);
-}
+IJLERR __stdcall ijlInitWrapper(JPEG_CORE_PROPERTIES *jcprops) { return ijlInit(jcprops); }
 
-IJLERR __stdcall ijlFreeWrapper(JPEG_CORE_PROPERTIES *jcprops) {
-    return ijlFree(jcprops);
-}
+IJLERR __stdcall ijlFreeWrapper(JPEG_CORE_PROPERTIES *jcprops) { return ijlFree(jcprops); }
 
 IJLERR __stdcall ijlReadWrapper(JPEG_CORE_PROPERTIES *jcprops, IJLIOTYPE iotype) {
     return ijlRead(jcprops, iotype);
@@ -21,6 +15,4 @@ IJLERR __stdcall ijlWriteWrapper(JPEG_CORE_PROPERTIES *jcprops, IJLIOTYPE iotype
     return ijlWrite(jcprops, iotype);
 }
 
-const char *__stdcall ijlErrorStrWrapper(IJLERR code) {
-    return ijlErrorStr(code);
-}
+const char *__stdcall ijlErrorStrWrapper(IJLERR code) { return ijlErrorStr(code); }
